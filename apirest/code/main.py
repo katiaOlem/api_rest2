4900cb08399e790276d4b588c43af77e3c6a8915
katiaOlem <1719110962@utectulancingo.edu.mx>
2022-06-21 04:52:42 +0000

    Funciona

--- apirest/code/main.py
+++ apirest/code/main.py
@@ -1,39 +1,76 @@
-from fastapi import FastAPI
-from typing import Union
-import sqlite3
-from typing import List
-from pydantic import BaseModel
-
-class Respuesta(BaseModel):
-    message: str
-    
-class Cliente(BaseModel):
-    id_cliente: int
+from fastapi import FastAPI 
+from typing import Union 
+import sqlite3 
+from typing import List 
+from pydantic import BaseModel 
+  
+class Respuesta (BaseModel) : 
+     message: str 
+          
+  
+class Cliente (BaseModel): 
+     id_cliente: int 
+     nombre: str 
+     email: str 
+  
+class ClienteID(BaseModel):
     nombre: str
     email : str
 
-app = FastAPI()
 
-@app.get("/", response_model=Respuesta)
-async def index():
-    return  {"message":"API REST"}
-      
+app =FastAPI() 
+  
+@app.get("/", response_model=Respuesta) 
+async def index(): 
+    return {"message": "API REST"} 
+  
+@app.get("/clientes/") 
+async def clientess(): 
+     with sqlite3.connect('sql/clientes.sqlite') as connection: 
+         connection.row_factory = sqlite3.Row 
+         cursor=connection.cursor() 
+         cursor.execute("SELECT * FROM clientes") 
+         response=cursor.fetchall() 
+         return response 
+         return {"message": "API REST"} 
+  
+@app.get("/clientes/{id}") 
+async def clientes(id): 
+     with sqlite3.connect('sql/clientes.sqlite') as connection: 
+         connection.row_factory = sqlite3.Row 
+         cursor=connection.cursor() 
+         cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id))) 
+         response=cursor.fetchall() 
+         return response
 
-@app.get("/clientes/", response_model=List[Cliente])
-async def clientes():
-    with sqlite3.connect('code/sql/clientes.sqlite') as connection:
+@app.post("/clientes/", response_model=Respuesta)
+def post_cliente(cliente: ClienteID):
+    with sqlite3.connect('sql/clientes.sqlite') as connection:
         connection.row_factory = sqlite3.Row
         cursor=connection.cursor()
-        cursor.execute("SELECT * FROM clientes")
-        response=cursor.fetchall()
+        cursor.execute("INSERT INTO clientes(nombre,email) VALUES(?,?)", (cliente.nombre,cliente.email))
+        cursor.fetchall()
+        response = {"message":"Cliente agregado"}
         return response
 
-        
-@app.get("/clientes/{id}", response_model=List[Cliente])
-async def clientes(id):
-    with sqlite3.connect('code/sql/clientes.sqlite') as connection:
+
+@app.put("/clientes/", response_model=Respuesta) #agregar
+async def clientes_update(nombre: str="", email:str="", id_cliente:int=0):
+    with sqlite3.connect("sql/clientes.sqlite") as connection:
         connection.row_factory = sqlite3.Row
-        cursor=connection.cursor()
-        cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id)))
-        response=cursor.fetchall()
+        cursor = connection.cursor()
+        cursor.execute("UPDATE clientes SET nombre =?, email= ? WHERE id_cliente =?;",(nombre, email, id_cliente))
+        cursor.fetchall()
+        response = {"message":"Cliente actualizado"}
         return response
+
+
+@app.delete("/clientes/{id}")  #eliminar
+async def clientes(id): 
+     with sqlite3.connect('sql/clientes.sqlite') as connection: 
+         connection.row_factory = sqlite3.Row 
+         cursor=connection.cursor() 
+         cursor.execute("DELETE FROM clientes WHERE id_cliente={}".format(int(id))) 
+         cursor.fetchall() 
+         response = {"message":"Cliente borrado"}
+         return response
--- apirest/test/test_main.py
+++ apirest/test/test_main.py
@@ -1,28 +1,49 @@
-
 from fastapi.testclient import TestClient
 from code.main import app
-import requests
-
 clientes = TestClient(app)
 
 
 
 def test_index():
-    response = clientes.get("/") #request#
-    data = {"message":"API REST"}
-    assert response.status_code == 200 ##petición enviada ok
+    response= clientes.get("/") 
+    data = {"message": "API REST"}
+    assert response.status_code == 200
+    assert response.json() == data
+
+
+
+def test_clientes():
+    response = clientes.get("/clientes/") 
+    data = [{"id_cliente":1,"nombre":"Katia","email":"kat160417@gmail.com"},
+    {"id_cliente":2,"nombre":"Itzel","email":"itezel16@gmail.com"},
+    {"id_cliente":3,"nombre":"Leo","email":"leo17@gmail.com"}]
+    assert response.status_code == 200
     assert response.json() == data
 
 def test_clientes():
-    response = clientes.get("/clientes/")  
-    data =[{"id_cliente":1,"nombre":"Katia","email":"kat160417@gmail.com"},
-    {"id_cliente":2,"nombre":"Itzel","email":"itezel16@gmail.com"},
-    {"id_cliente":3,"nombre":"Leo","email":"leo17@gmail.com"}]
+    response = clientes.get("/clientes/1") 
+    data = [{"id_cliente":1,"nombre":"Katia","email":"kat160417@gmail.com"}]
+    assert response.status_code == 200
+    assert response.json() == data
+
+
+def test_clientes_post():
+    payload={ "id_cliente": 4, "nombre": "str", "email" : "str"}
+    response = clientes.post("/clientes/", json=payload)  
+    data = {"message":"Cliente insertado"}
     assert response.status_code ==200
     assert response.json()==data
 
-def test_clientes():
-    response = clientes.get("/clientes/1")  
-    data =[{"id_cliente":1,"nombre":"Katia","email":"kat160417@gmail.com"}]
+def test_clientes_update():
+    payload={ "id_cliente": 4, "nombre": "Cliente actualizado", "email" : "actualizado@email.com"}
+    response = clientes.put("/clientes/", json=payload)
+    data = {"message":"Cliente actualizado"}
     assert response.status_code ==200
     assert response.json()==data
+    
+def test_clientes_delete():
+    payload={ "id_cliente": 4, "nombre": "Cliente actualizado", "email" : "actualizado@email.com"}
+    response = clientes.delete("/clientes/4", json=payload)
+    data = {"message": "Cliente eliminado"}
+    assert response.status_code == 200
+    assert response.json() == data
